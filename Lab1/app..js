4941cd3633c97c97e0435cd0860e33c815f9bfe5
thanhfpl <thanhfplhn@gmail.com>
2024-11-05 21:31:27 +0700

    Initial commit

---
+++ Lab1/app..js
@@ -0,0 +1,18 @@
+//tao giá trị cho biến a 
+let a = 10;
+
+// Hiển thị giá trị của a trong console và alert
+console.log(a);
+alert(a);
+
+
+// Nhập dữ liệu
+var n = prompt("Input n: "); // Yêu cầu người dùng nhập giá trị n
+
+// Xuất dữ liệu ra màn hình
+document.write(n); // Hiển thị giá trị n trên màn hình
+
+
+
+
+//Bài 4 + 5
